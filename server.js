b956bf85f576a0e7478df1a57c9e60a55554118b
Diego Bermudez Rz <dieguito.1743@gmail.com>
2017-11-30 00:19:54 -0500

    Cambios en server.js

--- server.js
+++ server.js
@@ -127,7 +127,17 @@ app.post('/registro', function (req, res) {
 });
 
 app.post('/consulta', function (req, res) {
-  res.send('DIEGO DAVID BERMUDEZ RODRIGUEZ 74036752');
+   if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    db.collection('postulantes').find({}).toArray(function(err, docs){
+      res.send(docs);
+      callback(docs);
+    });
+  } else {
+    res.send('{ pageCount: -1 }');
+  }
 });
 
 app.post('/consultaDNI', function (req, res) {
